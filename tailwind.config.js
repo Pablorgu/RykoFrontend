8c04dd5ef1bdb3b7ae619ba9807bc97e2c54a0b9
pablorgu <pablor623@gmail.com>
2025-07-28 19:38:56 +0200

    feat: add dark mode support and new dependencies
    - Enable dark mode in tailwind config
    - Add @gorhom/bottom-sheet, expo-image-picker, and react-native-gesture-handler dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./app/**/*.{js,jsx,ts,tsx}",
     "./App.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class', 
   presets: [require("nativewind/preset")],
   theme: {
     extend: {},
